06f808a8e3bedfdab906b2092024fcadc4d34005
Gundy <korea1880@naver.com>
2023-09-16 21:21:27 +0900

    test: CalculatorViewModel을 검증하기 위한 테스트 케이스 작성

--- Gunculator/GunculatorTests/CalculatorViewModelTests.swift
+++ Gunculator/GunculatorTests/CalculatorViewModelTests.swift
@@ -17,4 +17,90 @@ final class CalculatorViewModelTests: XCTestCase {
         
         sut = nil
     }
+    
+    func test_input에_5를전달하면_inputted가5가된다() {
+        // given
+        sut = CalculatorViewModel()
+        
+        // when
+        sut.input("5")
+        
+        // then
+        XCTAssertEqual(sut.inputted, "5")
+    }
+    
+    func test_input에_차례대로1_2_3을전달하면_inputted가123이된다() {
+        // given
+        sut = CalculatorViewModel()
+        
+        // when
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        
+        // then
+        XCTAssertEqual(sut.inputted, "123")
+    }
+    
+    func test_inputted가123일때_operate에backspace를전달하면_inputted가12가된다() {
+        // given
+        sut = CalculatorViewModel()
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        
+        // when
+        sut.operate(by: .backspace)
+        
+        // then
+        XCTAssertEqual(sut.inputted, "12")
+    }
+    
+    func test_inputted가123일때_operate에signChange를전달하면_inputted가마이너스123이된다() {
+        // given
+        sut = CalculatorViewModel()
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        
+        // when
+        sut.operate(by: .signChange)
+        
+        // then
+        XCTAssertEqual(sut.inputted, "-123")
+    }
+    
+    func test_123을입력하고add를전달한뒤_123을입력하고equals를전달하면_inputted가246이된다() {
+        // given
+        sut = CalculatorViewModel()
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        sut.operate(by: .add)
+        
+        // when
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        sut.operate(by: .equals)
+        
+        // then
+        XCTAssertEqual(sut.inputted, "246")
+    }
+    
+    func test_divide를전달한뒤_0을입력하고equals를전달하면_inputted가NaN이된다() {
+        // given
+        sut = CalculatorViewModel()
+        for number in 1...3 {
+            sut.input("\(number)")
+        }
+        sut.operate(by: .divide)
+        
+        // when
+        sut.input("0")
+        sut.operate(by: .equals)
+        
+        // then
+        XCTAssertEqual(sut.inputted, "NaN")
+    }
 }
