790985b6b33dc1a9f01331957725a7d1818ead0f
Gundy <korea1880@naver.com>
2023-09-17 08:14:10 +0900

    feat: 편의를 위한 UIColor extension 추가

---
+++ Gunculator/Gunculator/Utility/Extension/UIColor+.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+.swift
+//  Gunculator
+//
+//  Created by Gundy on 2023/09/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let dot = UIColor(named: "dot")
+    static let numberPad = UIColor(named: "numberPad")
+    static let `operator` = UIColor(named: "operator")
+    static let upperPad = UIColor(named: "upperPad")
+}
