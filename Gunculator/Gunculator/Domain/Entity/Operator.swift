8fd0e27c40b586fb91d787bb26baf608eefa2729
Gundy <korea1880@naver.com>
2023-09-16 15:44:12 +0900

    feat: Operator case 및 원시값 지정

--- Gunculator/Gunculator/Domain/Entity/Operator.swift
+++ Gunculator/Gunculator/Domain/Entity/Operator.swift
@@ -5,4 +5,14 @@
 //  Created by Gundy on 2023/09/16.
 //
 
-enum Operaotor {}
+enum Operaotor: String {
+    
+    case clear = "C"
+    case backspace = "←"
+    case signChange = "⁺⁄₋"
+    case divide = "÷"
+    case multifly = "×"
+    case subtract = "-"
+    case add = "+"
+    case equals = "="
+}
