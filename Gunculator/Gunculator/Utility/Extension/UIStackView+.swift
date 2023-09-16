0dad7a5a374477248b49d1f4bd18a6ee313dc6d4
Gundy <korea1880@naver.com>
2023-09-17 08:17:45 +0900

    feat: extension을 통해 UIStackView에 편의 이니셜라이저 추가

---
+++ Gunculator/Gunculator/Utility/Extension/UIStackView+.swift
@@ -0,0 +1,24 @@
+//
+//  UIStackView+.swift
+//  Gunculator
+//
+//  Created by Gundy on 2023/09/17.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    convenience init(arrangedSubviews views: [UIView] = [],
+                     spacing: CGFloat = 0,
+                     axis: NSLayoutConstraint.Axis = .horizontal,
+                     alignment: UIStackView.Alignment = .fill,
+                     distribution: UIStackView.Distribution = .fill) {
+        self.init(arrangedSubviews: views)
+        
+        self.spacing = spacing
+        self.axis = axis
+        self.alignment = alignment
+        self.distribution = distribution
+    }
+}
