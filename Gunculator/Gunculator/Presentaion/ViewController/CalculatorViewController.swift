4ca6faf196445c9918996165cb1eaf2633e66d64
Gundy <korea1880@naver.com>
2023-09-17 09:20:13 +0900

    feat: 계산기 패드 UI 구현

--- Gunculator/Gunculator/Presentaion/ViewController/CalculatorViewController.swift
+++ Gunculator/Gunculator/Presentaion/ViewController/CalculatorViewController.swift
@@ -7,4 +7,104 @@
 
 import UIKit
 
-final class CalculatorViewController: UIViewController {}
+final class CalculatorViewController: UIViewController {
+    
+    private let viewModel: CalculatorViewModel = CalculatorViewModel()
+    private let padStackView: UIStackView = UIStackView(spacing: 8,
+                                                axis: .vertical,
+                                                        distribution: .fillEqually)
+    private lazy var firstRowStackView: UIStackView = UIStackView(arrangedSubviews: [makeOperatorButton("C", isUpperPad: true), makeOperatorButton("←", isUpperPad: true), makeOperatorButton("⁺⁄₋", isUpperPad: true), makeOperatorButton("÷")],
+                                                                  spacing: 8,
+                                                                  axis: .horizontal,
+                                                                  distribution: .fillEqually)
+    private lazy var secondRowStackView: UIStackView = UIStackView(arrangedSubviews: [makeNumberButton("7"), makeNumberButton("8"), makeNumberButton("9"), makeOperatorButton("×")],
+                                                                  spacing: 8,
+                                                                  axis: .horizontal,
+                                                                  distribution: .fillEqually)
+    private lazy var thirdRowStackView: UIStackView = UIStackView(arrangedSubviews: [makeNumberButton("4"), makeNumberButton("5"), makeNumberButton("6"), makeOperatorButton("-")],
+                                                                  spacing: 8,
+                                                                  axis: .horizontal,
+                                                                  distribution: .fillEqually)
+    private lazy var forthRowStackView: UIStackView = UIStackView(arrangedSubviews: [makeNumberButton("1"), makeNumberButton("2"), makeNumberButton("3"), makeOperatorButton("+")],
+                                                                  spacing: 8,
+                                                                  axis: .horizontal,
+                                                                  distribution: .fillEqually)
+    private lazy var fifthRowStackView: UIStackView = UIStackView(arrangedSubviews: [makeNumberButton("0"), makeDotButton(), makeOperatorButton("=")],
+                                                                  spacing: 8,
+                                                                  axis: .horizontal,
+                                                                  distribution: .fill)
+    private lazy var clearButton: UIButton = makeOperatorButton("C", isUpperPad: true)
+    private lazy var backspaceButton: UIButton = makeOperatorButton("←", isUpperPad: true)
+    private lazy var signChangeButton: UIButton = makeOperatorButton("⁺⁄₋", isUpperPad: true)
+    private lazy var divideButton: UIButton = makeOperatorButton("÷")
+    private lazy var multiflyButton: UIButton = makeOperatorButton("×")
+    private lazy var subtractButton: UIButton = makeOperatorButton("-")
+    private lazy var addButton: UIButton = makeOperatorButton("+")
+    private lazy var equalsButton: UIButton = makeOperatorButton("=")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewHierarchy()
+        configureConstraints()
+    }
+    
+    private func configureViewHierarchy() {
+        view.addSubview(padStackView)
+        [firstRowStackView, secondRowStackView, thirdRowStackView, forthRowStackView, fifthRowStackView].forEach({ padStackView.addArrangedSubview($0) })
+    }
+    
+    private func configureConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        guard let first = firstRowStackView.arrangedSubviews.first,
+              let last = fifthRowStackView.arrangedSubviews.last,
+              let zero = fifthRowStackView.arrangedSubviews.first else { return }
+        
+        NSLayoutConstraint.activate([
+            padStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            padStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            padStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+            first.widthAnchor.constraint(equalTo: first.heightAnchor),
+            last.widthAnchor.constraint(equalTo: last.heightAnchor),
+            zero.widthAnchor.constraint(equalTo: last.widthAnchor, multiplier: 2, constant: 8)
+        ])
+    }
+    
+    private func makeOperatorButton(_ sign: String, isUpperPad: Bool = false) -> UIButton {
+        return UIButton(primaryAction: makeOperatorAction(sign),
+                        font: .preferredFont(forTextStyle: .largeTitle),
+                        titleColor: isUpperPad ? .systemBackground : .white,
+                        backgroundColor: isUpperPad ? .upperPad : .operator)
+    }
+    
+    private func makeOperatorAction(_ sign: String) -> UIAction? {
+        guard let `operator` = Operator(rawValue: sign) else { return nil }
+        let action = UIAction(title: sign) { [weak self] _ in
+            self?.viewModel.operate(by: `operator`)
+        }
+        
+        return action
+    }
+    
+    private func makeNumberButton(_ number: String) -> UIButton {
+        return UIButton(primaryAction: makeOperandAction(number),
+                        font: .preferredFont(forTextStyle: .largeTitle),
+                        titleColor: .black,
+                        backgroundColor: .numberPad)
+    }
+    
+    private func makeDotButton() -> UIButton {
+        return UIButton(primaryAction: makeOperandAction("."),
+                        font: .preferredFont(forTextStyle: .largeTitle),
+                        titleColor: .label,
+                        backgroundColor: .dot)
+    }
+    
+    private func makeOperandAction(_ input: String) -> UIAction {
+        let action = UIAction(title: input) { [weak self] _ in
+            self?.viewModel.input(input)
+        }
+        
+        return action
+    }
+}
